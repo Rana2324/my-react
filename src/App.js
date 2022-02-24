596dd6737f3ef8c17c8339293a05d2a4703f9644
Rana2324 <shohelranatonmoy@gmail.com>
2022-02-24 16:30:47 +0900

    fetch data and show ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter></Counter> */}
+      <ExterNalUsers></ExterNalUsers>
     </div>
   );
 }
+
+function ExterNalUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect (() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>External Users</h2>
+      
+      {
+        users.map(user => <User name={user.name} email= {user.email}></User>)
+
+      }
+    </div>
+  );
+}
+function User(props){
+  return(
+    <div className="user">
+        <h2>Name: {props.name}</h2>
+        <p>Email: {props.email}</p>
+    </div>
+  )
+}
+
+
 
 export default App;
